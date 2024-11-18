d9d7bff823aee048e84aec6534dd7f111ed0b66f
cuonghx <cuonghx@gmail.com>
2024-11-18 15:34:27 +0700

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap(): Promise<void> {
   const applicationPort: string =
     configurationService.getOrThrow('application.port');
 
+  app.enableCors();
   await app.listen(applicationPort);
 }
 
